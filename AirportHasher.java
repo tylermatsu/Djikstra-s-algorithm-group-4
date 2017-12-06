d80a462fedcb903c52eca6cc97649ce0baca6c79
mantinglin <mantinglin23@gmail.com>
2017-12-05 21:59:47 -0800

    Delete AirportHasher.java

--- AirportHasher.java
+++
@@ -1,7 +0,0 @@
-package grp;
-
-public class AirportHasher implements Hasher<Location> {
-	public int hash(Location myAirport) {
-		return (myAirport.getAirportName() != null) ? myAirport.getAirportName().hashCode() : 0;
-	}
-}
